7379ce156699618c5aa50f8fd5102ce4dfe154ce
Shivathamme <147991564+Shivathamme@users.noreply.github.com>
2026-04-25 18:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import os
 import re
 
 # Fix import path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.getcwd())
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
 
